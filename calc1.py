611f216ffc1b4370f4f988acbe7deed3a4ad1a48
m2503908 <m2503908@edu.misis.ru>
2025-11-10 15:30:16 +0300

    Create calc1.py

---
+++ calc1.py
@@ -0,0 +1,12 @@
+num1 = int(input())
+num2 = int(input())
+oper = input()
+
+if oper == '+':
+    print(f"{num1} {oper} {num2} = {num1 + num2}")
+elif oper  == '-':
+    print(f"{num1} {oper} {num2} = {num1 - num2}")
+elif oper  == '*':
+    print(f"{num1} {oper} {num2} = {num1 * num2}")
+elif oper  == '/':
+    print(f"{num1} {oper} {num2} = {num1 / num2}")
