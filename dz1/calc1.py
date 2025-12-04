0ce16465c99f689d963cf8f5b6a4281468eaab68
m2503908 <m2503908@edu.misis.ru>
2025-12-04 21:40:13 +0300

    Create calc1.py

---
+++ dz1/calc1.py
@@ -0,0 +1,12 @@
+num1 = float(input())
+num2 = float(input())
+oper = input()
+
+if oper == '+':
+    print(f"{num1} {oper} {num2} = {num1 + num2}")
+elif oper  == '-':
+    print(f"{num1} {oper} {num2} = {num1 - num2}")
+elif oper  == '*':
+    print(f"{num1} {oper} {num2} = {num1 * num2}")
+elif oper  == '/':
+    print(f"{num1} {oper} {num2} = {num1 / num2}")
